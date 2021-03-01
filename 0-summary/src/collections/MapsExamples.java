e88eb1298f6b762a6e7d6484dae8ad89f7cd5232
FinalAngel <angelo.dini@divio.ch>
2021-03-01 10:05:41 +0100

    add more map data :D

--- 0-summary/src/collections/MapsExamples.java
+++ 0-summary/src/collections/MapsExamples.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapsExamples {
   
@@ -8,6 +9,7 @@ public class MapsExamples {
   }
 
   private void mapsExamples() {
+    // we use Double instead of double because it might return null
     Map<String, Double> salary = new HashMap<>();
     salary.put("Max", 80000.0);
     salary.put("Muster", 120000.0);
@@ -15,6 +17,20 @@ public class MapsExamples {
     System.out.println(salary);
     // creates key/value pairs
     // [{ "Max": 80000.0 }, { "Muster": 120000.0 }]
+
+    Double maxSalary = salary.get("Max"); // provides the value or null
+    if (maxSalary != null) {
+      System.out.println(maxSalary);
+    }
+
+    // to iterate use entrySet to generate a list on the fly from a map
+    Set<Map.Entry<String, Double>> entries = salary.entrySet();
+    // operations on the entrySet will affect the map itself!!!
+    for (Map.Entry<String, Double> entry : entries) {
+      String name = entry.getKey();
+      double lohn = entry.getValue();
+      System.out.println(name + ": " + lohn);
+    }
   }
 
 }
